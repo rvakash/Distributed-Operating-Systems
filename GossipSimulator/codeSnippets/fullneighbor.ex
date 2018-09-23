c7d1cba6ad288d992f08bcca0bdbcce248cf726f
adityabhanje <adityabhanje@gmail.com>
2018-09-23 19:48:28 -0400

    getLineNeighbors

--- GossipSimulator/codeSnippets/fullneighbor.ex
+++ GossipSimulator/codeSnippets/fullneighbor.ex
@@ -1,8 +1,19 @@
 defmodule FullNeighbor do
-  def getNeighborsFull(actor,numNodes) do
-    range=1..numNodes
+  def getLineNeighbor(nodeId,numOfNodes) do
+    range =
+      cond do
+        nodeId == 1 -> [2]
+        nodeId == numOfNodes -> [numOfNodes-1]
+        nodeId >1 and nodeId < numOfNodes -> [nodeId-1,nodeId+1]
+      end
+    # IO.inspect(range)
+  end
+
+
+  def getNeighborsFull(nodeId,numOfNodes) do
+    range=1..numOfNodes
     range
-    |> Enum.filter(fn(value) -> value != actor end)
+    |> Enum.filter(fn(value) -> value != nodeId end)
     |> Enum.map(fn(filtered_value) -> filtered_value * 1 end)
     # IO.inspect Neighboringlist
   end
