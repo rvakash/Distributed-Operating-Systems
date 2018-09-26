183dc3d6b73cae8b824e5649fa5236703f3de1ff
adityabhanje <adityabhanje@gmail.com>
2018-09-25 20:51:25 -0400

    Added Imperfect Line Neighbors

--- GossipSimulator/codeSnippets/fullneighbor.ex
+++ GossipSimulator/codeSnippets/fullneighbor.ex
@@ -1,4 +1,6 @@
 defmodule FullNeighbor do
+
+  #Getting the line neighbor
   def getLineNeighbor(nodeId,numOfNodes) do
     range =
       cond do
@@ -9,12 +11,29 @@ defmodule FullNeighbor do
     # IO.inspect(range)
   end
 
-
+  #Getiing the full neighbors
   def getNeighborsFull(nodeId,numOfNodes) do
     range=1..numOfNodes
     range
     |> Enum.filter(fn(value) -> value != nodeId end)
     |> Enum.map(fn(filtered_value) -> filtered_value * 1 end)
     # IO.inspect Neighboringlist
+  end
+  #Getting the imperfect Line Neighbors
+  def getImperfectLineNeighbor(nodeId,numOfNodes) do
+    range =
+      cond do
+        nodeId == 1 -> [2,:rand.uniform(numOfNodes-2)+2]
+        nodeId == numOfNodes -> [numOfNodes-1,:rand.uniform(numOfNodes-2)]
+        nodeId >1 and nodeId < numOfNodes -> randomNo(nodeId,numOfNodes)
+      end
+  end
+ #random neighbor generation
+  def randomNo(nodeId,numOfNodes) do
+    x=:rand.uniform(numOfNodes)
+    cond do
+      x==(nodeId-1) or x==(nodeId+1) -> randomNo(nodeId,numOfNodes)
+      true-> [nodeId-1,nodeId+1,x]
+    end
   end
 end
