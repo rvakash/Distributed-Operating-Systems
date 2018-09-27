992acb8189523aab3a94139b16c620f262efb293
rvakash <rvakash@gmail.com>
2018-09-27 19:16:59 -0400

    Added Actor

--- GossipSimulator/lib/actor.ex
+++ GossipSimulator/lib/actor.ex
@@ -2,8 +2,13 @@ defmodule Actor do
     use GenServer
 
     def init([nodeId, neighborList, algorithm]) do
+        # Map.put(state, "neighborList", neighborList)
         {:ok, [Active,nodeId, neighborList, algorithm, 0]}#nodeId, neighborList, algorithm, receivedCount
     end
 
-    
+    def handle_cast({:message, rumour}, state) do
+        {:ok, count} = Map.fetch(state, "count")
+        state = Map.put(state, "count", count + 1)
+
+    end
 end
--- GossipSimulator/lib/proj2.ex
+++ GossipSimulator/lib/proj2.ex
@@ -4,24 +4,28 @@
 # Description: Use Actor Model(Genserver) in Elixir to 
 #####################################################
 defmodule Proj2 do
+# Instructions to run the project
+# 1) $mix escript.build
+# 2) $escript proj2 100 full gossip
+
     def main(args) do
         # Receive total number of nodes, topology, algorithm, triggerNodes(optional), threshold(optional) from user.
         # Read README.md for more details
         numOfNodes = String.to_integer(Enum.at(args,0))
         topology = Enum.at(args,1)
         algorithm = Enum.at(args,2)
-        triggerNodes = 
-            if Enum.at(args, 3) != nil do
-                String.to_integer(Enum.at(args, 3))
-            else
-                1
-            end
-        threshold = 
-            if Enum.at(args, 4) != nil do
-                String.to_integer(Enum.at(args, 4))
-            else
-                0
-            end
+        # triggerNodes = 
+        #     if Enum.at(args, 3) != nil do
+        #         String.to_integer(Enum.at(args, 3))
+        #     else
+        #         1
+        #     end
+        # threshold = 
+        #     if Enum.at(args, 4) != nil do
+        #         String.to_integer(Enum.at(args, 4))
+        #     else
+        #         0
+        #     end
         numOfNodes = if String.contains?(topology, "2d"), do: round(:math.pow(round(:math.sqrt(numOfNodes)), 2)), else: numOfNodes
 
         case topology do
@@ -30,33 +34,35 @@ defmodule Proj2 do
                     neighborList = getNeighborsFull(nodeId, numOfNodes)
                     GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
                 end
-            "3DGrid"        ->
-                Enum.each 1..numOfNodes, fn nodeId ->
-                    neighborList = getNeighbors3DGrid(nodeId, numOfNodes)
-                    GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
-                end
-            "2DGrid"        ->
-                Enum.each 1..numOfNodes, fn nodeId ->
-                    neighborList = getNeighbors2DGrid(nodeId, numOfNodes)
-                    GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
-                end
-            "3DTorus"       ->
-                Enum.each 1..numOfNodes, fn nodeId ->
-                    neighborList = getNeighbors3DTorus(nodeId, numOfNodes)
-                    GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
-                end
-            "line"          ->
-                Enum.each 1..numOfNodes, fn nodeId ->
-                    neighborList = getNeighborsLine(nodeId, numOfNodes)
-                    GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
-                end
-            "imperfectLine" ->
-                Enum.each 1..numOfNodes, fn nodeId ->
-                    neighborList = getNeighborsImperfectLine(nodeId, numOfNodes)
-                    GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
-                end
+            # "3DGrid"        ->
+            #     Enum.each 1..numOfNodes, fn nodeId ->
+            #         neighborList = getNeighbors3DGrid(nodeId, numOfNodes)
+            #         GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
+            #     end
+            # "2DGrid"        ->
+            #     Enum.each 1..numOfNodes, fn nodeId ->
+            #         neighborList = getNeighbors2DGrid(nodeId, numOfNodes)
+            #         GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
+            #     end
+            # "3DTorus"       ->
+            #     Enum.each 1..numOfNodes, fn nodeId ->
+            #         # generate3DTorus(numOfNodes);
+            #         neighborList = getNeighbors3DTorus(nodeId, numOfNodes)
+            #         GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
+            #     end
+            # "line"          ->
+            #     Enum.each 1..numOfNodes, fn nodeId ->
+            #         neighborList = getNeighborsLine(nodeId, numOfNodes)
+            #         GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
+            #     end
+            # "imperfectLine" ->
+            #     Enum.each 1..numOfNodes, fn nodeId ->
+            #         neighborList = getNeighborsImperfectLine(nodeId, numOfNodes)
+            #         GenServer.start_link(Actor, [nodeId, neighborList, algorithm], name: nodeId)
+            #     end
         end
 
+        GenServer.cast(1, {:message, "This is Elixir Gossip Simulator"})
     end
 
     def getNeighborsFull(nodeId,numOfNodes) do
