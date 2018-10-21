8f3e5be05d888c922beba1260183d93ad533c9a0
rvakash <rvakash@gmail.com>
2018-10-20 20:06:18 -0400

    Updated Actor

--- ChordProtocol/lib/actor.ex
+++ ChordProtocol/lib/actor.ex
@@ -1,20 +1,33 @@
 defmodule Actor do
     use GenServer
 
-    def init([nodeId, neighborList, algorithm, parentPID]) do
+    def init([nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes]) do
 
+        {:ok, {nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes}}
     end
 
     def handle_cast({:message, rumour}, state) do
+        {nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes} = state
 
+        {:noreply, {nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes}}
     end
 
     def handle_cast({:message_pushsum, news, neww}, state) do
+        {nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes} = state
 
+        {:noreply, {nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes}}
+    end
+
+    def handle_cast({:message_pushsum, news, neww}, state) do
+        {nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes} = state
+
+        {:noreply, {nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes}}
     end
 
     def handle_call(:is_active , _from, state) do
+        {nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes} = state
 
+        {:noreply, {nodeId, keys, fingerTable, successor, prevNodeId, numOfNodes}}
     end
 
     def handle_info(:kill_me_pls, state) do
