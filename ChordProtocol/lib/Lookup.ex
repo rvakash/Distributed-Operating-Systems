1fc0e7e24af148889906ba72692a3d6edecfdc51
rvakash <rvakash@gmail.com>
2018-10-20 23:36:58 -0400

    Updated loopup

--- ChordProtocol/lib/Lookup.ex
+++ ChordProtocol/lib/Lookup.ex
@@ -10,18 +10,18 @@ defmodule Lookup do
 
     def find_successor(keyID, nodeId, successor, fingerTable) do
         if(nodeId<keyID and keyID<successor) do
-        successor
+            successor
         else
-        nextID=closest_preceding_node(keyID, nodeId, fingerTable)
-        nextID.find_successor(keyID) #################Genserver call to the nextID ka lookup
+            nextID=closest_preceding_node(keyID, nodeId, fingerTable)
+            nextID.find_successor(keyID) #################Genserver call to the nextID ka lookup
         end
     end
 
     def closest_preceding_node(keyID, nodeID, fingerTable) do
         nodeID = for i <- (length(fingerTable)-1)..0 do
-        if elem(List.pop_at(fingerTable, i), 0) <= keyID do
-            elem(List.pop_at(fingerTable, i), 0)
-        end
+            if elem(List.pop_at(fingerTable, i), 0) <= keyID do
+                elem(List.pop_at(fingerTable, i), 0)
+            end
         end
         nodeIdfinal=List.to_integer(Enum.filter(nodeID, fn(elem) -> elem != nil end))
     end
